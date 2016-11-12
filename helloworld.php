d42d8cc7dd9e0d3eaa16372d0270a4f0eeaec5f6
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-11-12 15:55:22 -0800

    add simple hello word page

---
+++ helloworld.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <title>Hello World!</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
